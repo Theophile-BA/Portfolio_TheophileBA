2c0f2cfe6236561ef62af268361034d8f5277f7d
Theophile-BA <contact.theophileboniface@gmail.com>
2023-10-08 16:52:26 +0200

    :smiley: first push :smiley:

--- app/Portfolio/page.tsx
+++ app/Portfolio/page.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
 
 export default function Layout() {
+
   return (
     <>
-      <div className="bg-red-500 text-black m-5 p-5">
+      <div className="bg-yellow-800 p-6">
         <div>Hello world ! I'm Théophile !</div>
       </div>
     </>
